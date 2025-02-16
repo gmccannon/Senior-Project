ace13d6751b4b69a4884dbb15ae899ec7052476f
Elia Albaba <ea152@uakron.edu>
2025-02-15 22:50:42 -0500

    Add mock api calls

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -16,27 +16,19 @@ const Home = () => {
     try {
       const response = await fetch(`/api/search?query=${query}`);
       const data: { results: SearchResult[] } = await response.json();
+      console.log('Search results:', data.results);
       setResults(data.results);
+
     } catch (error) {
       console.error('Error fetching search results:', error);
     }
-  };
-  const handleSearchTest = async (query: string): Promise<void> => {
-    // Hardcoded data for demonstration purposes
-    const hardcodedResults: SearchResult[] = [
-      { title: 'Result 1', description: 'Description for result 1' },
-      { title: 'Result 2', description: 'Description for result 2' },
-      { title: 'Result 3', description: 'Description for result 3' },
-    ];
-    setResults(hardcodedResults);
   };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Nudle</h1>
       <div className={styles.searchBar}>
-        {/* <SearchBar onSearch={handleSearch} /> */}
-        <SearchBar onSearch={handleSearchTest} />
+        <SearchBar onSearch={handleSearch} />
       </div>
       <div className={styles.results}>
         {results.length > 0 ? (
