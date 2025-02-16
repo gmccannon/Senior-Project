35930603785631576c75b62e4e13dd9d02f986a9
Elia Albaba <ea152@uakron.edu>
2025-02-15 22:32:38 -0500

    Fix formatting of search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -14,12 +14,12 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({ onSearch }
     return (
         <form onSubmit={handleSubmit}>
             <input
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="Search..."
-                required
-                className="flex-grow p-2 border border-gray-300 rounded-l-md"
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search..."
+            required
+            className="flex-grow p-2 border border-gray-300 rounded-l-md text-black placeholder-gray-500"
             />
             <button type="submit" className="p-2 bg-blue-500 text-white rounded-r-md ml-2">Search</button>
         </form>
