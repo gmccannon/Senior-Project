f9601a5889206cc7a97f342f5a3b38a2c664e5c7
gmccannon <gwm13@uakron.edu>
2025-03-08 20:14:59 -0500

    Add arxiv api route and article summaries

--- frontend/app/(overview)/articles/page.tsx
+++ frontend/app/(overview)/articles/page.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 
 const Articles = () => {
     const [articleResults, setArticleResults] = useState<ArxivArticle[]>([]);
+    const [articleAIsummary, setArticleAIsummary] = useState<string>("Click an article to get a summary");
     const [articlesLoading, setArticlesLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
@@ -33,23 +34,30 @@ const Articles = () => {
     }, [searchTerm]);
 
   return (
-    <div className="bg-neutral-900 min-h-screen mt-4 pl-20">
-        {articlesLoading && <p className="text-white">Loading articles...</p>}
-        {error && <p className="text-red-500">{error}</p>}
-        {!articlesLoading && !error && articleResults.length === 0 && searchTerm && (
-          <p className="text-gray-400">No results found for search "{searchTerm}"</p>
-        )}
-
+    <div className="bg-neutral-900 min-h-screen mt-4 pl-20 flex gap-10">
+        {/* Left Column - Search Results */}
         {/* TODO: Add pagination */}
-        {!articlesLoading &&
-          articleResults.map((result, index) => (
-            <div key={index} className="mb-6">
-              <Link href={result.link} className="text-xl text-blue-500 hover:underline break-all">
-                {result.link.slice(0,50)}
-              </Link>
-              <p className="text-white">{result.title}</p>
-            </div>
-          ))}
+        <div className="w-1/2">
+            {articlesLoading && <p className="text-white">Loading articles...</p>}
+            {error && <p className="text-red-500">{error}</p>}
+            {!articlesLoading && !error && articleResults.length === 0 && searchTerm && (
+            <p className="text-gray-400">No results found for search "{searchTerm}"</p>
+            )}
+            {!articlesLoading && articleResults.map((result, index) => (
+                <div key={index} className="mb-6" onClick={() => setArticleAIsummary(result.summary)}>
+                <Link href={result.siteLink.replace(".org/abs/", ".org/pdf/")} className="text-xl text-blue-500 hover:underline break-all">
+                    {result.siteLink.replace(".org/abs/", ".org/pdf/")}
+                </Link>
+                <p className="text-white">{result.title}</p>
+                </div>
+            ))}
+        </div>
+
+        {/* Right Column - Article Summary */}
+        <div className=" border-l border-gray-700 pl-6 w-1/2">
+            <h2 className="text-xl font-semibold text-white">AI Assistant</h2>
+            <p className="text-gray-400">{articleAIsummary}</p>
+        </div>
     </div>
   )
 }
